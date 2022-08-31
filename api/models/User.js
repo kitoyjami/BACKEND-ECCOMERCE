dae3c5500638059452a299ed9eaa83234b9a414e
kitoyjami <weurbanor@unitru.edu.pe>
2022-08-30 22:44:05 -0500

    feat model user created

---
+++ api/models/User.js
@@ -0,0 +1,71 @@
+import mongoose from "mongoose"
+
+/**
+ * Cosas necesarias para crear un modelo
+ * 1. schema
+ * 2. nombre
+ */
+
+const schema = new mongoose.Schema({
+
+    nombre:{
+        type: String,
+        required: true
+    },
+    apellido:{
+        type: String,
+        required: true
+    },
+    direccion: {
+        type: String,
+        required: true
+    },
+    calle:{
+        type: String,
+        required: true
+    },
+    localidad:{
+        type: String,
+
+    },
+    nroExterior:{
+        type: String,
+    },
+    nroInterior:{
+        type: String,
+    },
+    entreCalle:{
+        type: String,
+    },
+    ciudad:{
+        type: String,
+    },
+    pais:{
+        type: String,
+    },
+    codigoPostal:{
+        type: String,
+    },
+    referencias:{
+        type: String,
+    },
+    correo:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    telefono:{
+        type: String,
+        required: true
+    },
+    contacto:{
+        type: String,
+    }
+})
+
+
+
+export default mongoose.model("User", schema)
